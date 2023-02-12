03e54cd00bfd16fac2e15a7eecb60afbaec29210
0x <55315423+0xfff0800@users.noreply.github.com>
2023-02-12 21:10:58 +0300

    Update Parrot.sh

--- Parrot.sh
+++ Parrot.sh
@@ -1,3 +1,5 @@
+touch .bashrc
+
 # ~/.bashrc: executed by bash(1) for non-login shells.
 # see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 # for examples
@@ -10,6 +12,7 @@
 HISTCONTROL=ignoredups:ignorespace
 
 # append to the history file, don't overwrite it
+shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=1000
@@ -17,6 +20,7 @@ HISTFILESIZE=2000
 
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
+shopt -s checkwinsize
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
@@ -102,3 +106,4 @@ fi
 if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
     . /etc/bash_completion
 fi
+
