585edd7b065765903f3c898e2fd97f1f6c4651bc
0x <55315423+0xfff0800@users.noreply.github.com>
2023-02-17 17:36:39 +0300

    Update Parrot.sh

--- Parrot.sh
+++ Parrot.sh
@@ -11,15 +11,17 @@
 HISTCONTROL=ignoredups:ignorespace
 
 # append to the history file, don't overwrite it
-shopt -s histappend
-
+yellow=`tput setaf 3`
+magenta=`tput setaf 5`
+echo "${magenta}---------------------"
+echo "${yellow}[+] Parrot ish 1.0v"
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=1000
 HISTFILESIZE=2000
 
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
-shopt -s checkwinsize
+echo "${yellow}[+] Github 0xfff0800"
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
